e17001b0e8a2f93097cf4a39896d2721f52c4375
sherruh <sherruh@gmail.com>
2019-02-19 20:52:44 +0600

    format Task View Holder

--- app/src/main/java/com/example/recyclerview/adapters/TaskAdapter.java
+++ app/src/main/java/com/example/recyclerview/adapters/TaskAdapter.java
@@ -1,4 +1,32 @@
 package com.example.recyclerview.adapters;
 
-public class TaskAdapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.recyclerview.R;
+import com.example.recyclerview.viewholders.TaskViewHolder;
+
+public class TaskAdapter extends RecyclerView.Adapter<TaskViewHolder> {
+
+    @NonNull
+    @Override
+    public TaskViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v= LayoutInflater.from(viewGroup.getContext()).
+                inflate(R.layout.view_holder_task,viewGroup,false);
+        TaskViewHolder vh=new TaskViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TaskViewHolder taskViewHolder, int i) {
+        taskViewHolder.onBind("DEFAULT TEXT");
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
 }
--- app/src/main/java/com/example/recyclerview/viewholders/TaskViewHolder.java
+++ app/src/main/java/com/example/recyclerview/viewholders/TaskViewHolder.java
@@ -1,4 +1,22 @@
 package com.example.recyclerview.viewholders;
 
-public class TaskViewHolder {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.recyclerview.R;
+
+public class TaskViewHolder extends RecyclerView.ViewHolder {
+
+    private TextView title;
+
+    public TaskViewHolder(@NonNull View itemView) {
+        super(itemView);
+        title=itemView.findViewById(R.id.vh_task_Title);
+    }
+
+    public void onBind(String strTitle){
+        title.setText(strTitle);
+    }
 }
